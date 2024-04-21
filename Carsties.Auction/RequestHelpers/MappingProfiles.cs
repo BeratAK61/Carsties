f0742c59e8ddb82257f8e0836440fcc599be30f7
Berat Ak <brt_ak_61@hotmail.com>
2024-04-22 01:31:40 +0300

    Add AutoMapper Configuration for Auction

---
+++ Carsties.Auction/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Carsties.Auction.DTOs;
+using Carsties.Auction.Entities;
+
+namespace Carsties.Auction.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Carsties.Auction.Entities.Auction, AuctionDto>().IncludeMembers(x => x.Item);
+
+        CreateMap<Item, AuctionDto>();
+
+        CreateMap<CreateAuctionDto, Carsties.Auction.Entities.Auction>()
+            .ForMember(d => d.Item, o => o.MapFrom(s => s));
+
+        CreateMap<CreateAuctionDto, Item>();
+    }
+}
