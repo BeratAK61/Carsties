a09e3e062aece28cb38d9a4d4bc349c8007d1d02
Berat Ak <brt_ak_61@hotmail.com>
2024-08-12 12:29:29 +0300

    Add Consumer to Auction Service For Consuming Fault Queues

--- src/Services/Auction/Carsties.Auction/Program.cs
+++ src/Services/Auction/Carsties.Auction/Program.cs
@@ -1,3 +1,4 @@
+using Carsties.Auction.Consumers;
 using Carsties.Auction.Data;
 using MassTransit;
 using Microsoft.EntityFrameworkCore;
@@ -26,6 +27,10 @@ builder.Services.AddMassTransit(x =>
 
 
     });
+
+    x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
 
     x.UsingRabbitMq((context, cfg) =>
     {
